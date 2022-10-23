f56a47c0f8e831701ab0ab5275af72d7b12980e3
comsc107 <pukkum_@hotmail.com>
2022-10-23 12:26:32 +0700

    frist commit

---
+++ spec/package-test-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PackageTestView from '../lib/package-test-view';
+
+describe('PackageTestView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
